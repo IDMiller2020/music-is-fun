3aeba308299beb8a245d6f197d67313fea2741f3
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-04-08 15:24:46 -0600

    Errors fixed

--- app/Models/Song.js
+++ app/Models/Song.js
@@ -7,13 +7,13 @@ export default class Song {
     this.album = data.collectionName || data.album;
     this.price = data.trackPrice || data.price || 1;
     this.preview = data.previewUrl || data.preview;
-    this._id = data.trackId || data._id;
-    this.id = this._id
+    this.id = data.trackId || data.id;
+    //this.id = this._id
   }
 
   get Template() {
     return `
-    <div class="card" style="width: 18rem;" onclick="app.songsController.setActive(${this._id})">
+    <div class="card" style="width: 18rem;" onclick="app.songsController.setActive(${this.id})">
     <img src="${this.albumArt}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${this.artist} | ${this.title}</h5>
@@ -28,7 +28,7 @@ export default class Song {
     <img src="${this.albumArt}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${this.artist} | ${this.title}</h5>
-      <button type="button" class="btn btn-outline-danger" onclick="app.songsController.removeSong('${this._id}')">Remove</button>
+      <button type="button" class="btn btn-outline-danger" onclick="app.songsController.removeSong('${this.id}')">Remove</button>
     </div>
     </div>
         `;
@@ -45,7 +45,7 @@ export default class Song {
     <div>
       <audio controls><source src="${this.preview}" type="audio/ogg"</audio>
       </div>
-      <button type="button" class="btn btn-outline-success" onclick="app.songsController.addSong(${this._id})">Add To Playlist</button>
+      <button type="button" class="btn btn-outline-success" onclick="app.songsController.addSong(${this.id})">Add To Playlist</button>
     </div>
         `;
   }
